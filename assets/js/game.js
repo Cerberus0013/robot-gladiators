bba455bc095c42275ee60902b6c1d10e8550af78
Breedlove13 <b.breedlove@outlook.com>
2021-02-03 07:50:58 -0700

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 
 var enemyName = "Roborto";
@@ -15,6 +16,10 @@ var fight = function() {
 
 fight();
 
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'SKIP' to choose. ")
+
+if (promptFight === "fight"  || promptFight === "FIGHT") {
+
 //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 enemyHealth = enemyHealth - playerAttack;
   // Log a resulting message to the console so we know that it worked.
@@ -25,6 +30,7 @@ if (enemyHealth <= 0 ) {
 } else {
     window.alert(enemyName + " still has " + enemyHealth + " health left." );
 }
+
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
 playerHealth = playerHealth - enemyAttack;
   // Log a resulting message to the console so we know that it worked.
@@ -36,4 +42,15 @@ if (playerHealth <= 0) {
     window.alert(playerName + " has died!")
 } else {
     window.alert(playerName + " stil has " + playerHealth + " health left.");
+} 
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?")
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            playerMoney = playerMoney - 2;
+        } else {
+            fight()
+        }
+    } else {
+    window.alert("You need to choose a valid option. Try again!");
 }
