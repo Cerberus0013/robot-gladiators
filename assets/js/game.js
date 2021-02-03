ccc8fdf4bd0a4a411edde02313e7e353a5bbe8dc
Breedlove13 <b.breedlove@outlook.com>
2021-02-03 05:52:37 -0700

    JS fun

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("this is an alert! JS is running!" )
+// creates a function named "fight"
+
+function fight(){
+    window.alert("the fight has begun!");
+}
+
+// fight ();
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+
+console.log("this logs a string");
+
+console.log(10 + 10);
+console.log("our robot's name is " + playerName);
