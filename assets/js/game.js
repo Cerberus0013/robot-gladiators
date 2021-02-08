6b9d637bf779ea3f4da382cfc4f760228207651a
Breedlove13 <b.breedlove@outlook.com>
2021-02-08 06:03:20 -0700

    skip fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -58,26 +58,49 @@ var endGame = function() {
   }
 };
 
+var fightOrSkip = function () {
+    //ask fight if they want to skip or figh
+    promptFight = promptFight.toLowerCase();
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose." );
+            if (promptFight === "" || promptFight === null){
+                window.alert("Error, Please enter a valid reponse");
+               return fightOrSkip();
+            }
+            
+            if (promptFight === "skip" || promptFight === "SKIP"){
+                var confirmSkip = window.confirm("Are you sure you'd like to quit?")
+            
+            if (confimrSkip) {
+                window.alert(playerInfo.name + "has decided to skip this fight.")
+                playerInfo.playerMoney = Math.max(0, playerInfo.money - 10);
+                shop()
+            }
+    }
+}
+
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
   while (playerInfo.health > 0 && enemy.health > 0) {
     // ask player if they'd like to fight or run
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-
-    // if player picks "skip" confirm and then stop the loop
-    if (promptFight === 'skip' || promptFight === 'SKIP') {
-      // confirm player wants to skip
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-      // if yes (true), leave fight
-      if (confirmSkip) {
-        window.alert(playerName + ' has decided to skip this fight. Goodbye!');
-        // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
-        shop();
+    if (fightOrSkip()){
         break;
-      }
     }
+    // if player picks "skip" confirm and then stop the loo
+    // promptFight = promptFight.toLowerCase();
+
+    // if (promptFight === 'skip' || promptFight === 'SKIP') {
+    //   // confirm player wants to skip
+    //   var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    //   // if yes (true), leave fight
+    //   if (confirmSkip) {
+    //     window.alert(playerName + ' has decided to skip this fight. Goodbye!');
+    //     // subtract money from playerMoney for skipping
+    //     playerMoney = playerMoney - 10;
+    //     shop();
+    //     break;
+    //   }
+    };
 
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
@@ -110,7 +133,6 @@ var fight = function(enemy) {
       }
 
       // leave while() loop since enemy is dead
-      break;
     } else {
       window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
     }
@@ -138,8 +160,7 @@ var fight = function(enemy) {
     } else {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
-  }
-};
+  };
 
 // go to shop between battles function
 var shop = function() {
